5ba274b3c3ee83fb2ee7df0bcf937cab5d2a9a26
sweetamily <972041628@qq.com>
2018-11-19 11:49:49 +0800

    update

---
+++ src/components/index.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="manage_page fillcontain">
+    <el-col :span="4" style="height: 100vh;background-color:#545c64">
+        <el-menu
+        :default-active="$route.path"
+        class="el-menu-vertical-demo"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        router>
+        <el-menu-item index="statistics">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">统计中心</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+            <i class="el-icon-warning"></i>
+            <span slot="title">应急中心</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+            <i class="el-icon-document"></i>
+            <span slot="title">优惠券</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+            <i class="el-icon-service"></i>
+            <span slot="title">客服中心</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+            <i class="el-icon-sold-out"></i>
+            <span slot="title">财务中心</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+            <i class="el-icon-setting"></i>
+            <span slot="title">权限配置</span>
+        </el-menu-item>
+        </el-menu>
+    </el-col>
+    <el-col :span="20">
+       <div>{{$route.query.name}}</div>
+        <router-view></router-view>
+    </el-col>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="main">
+        <div class="contain">
+            <div class="demo-input-suffix">
+            属性方式：
+            <el-input
+                placeholder="请输入内容"
+                v-model="input21">
+            </el-input>
+            </div>
+            <div class="demo-input-suffix">
+            slot 方式：
+            <el-input
+                placeholder="请输入内容"
+                v-model="input23">
+            </el-input>
+            </div>
+            <el-button @click="go" type="primary">主要按钮</el-button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods:{
+  	go(){
+  		this.$router.push('/index')
+  	}
+  }
+}
+</script>
+<style>
+    .main{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        margin-top: 150px;
+    }
+    .contain{
+        width: 30vw;
+    }
+    .el-button{
+        width: 30vw;
+        margin-top: 40px;
+    }
+
+</style>
+
